3a3bfa0bd20e46956bb94213a0ed1cb95154dc12
Alex Elder <alex@handwritten.io>
2016-08-22 15:14:15 +0100

    Added package JSON and first test outline file.

---
+++ test/scramble.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var expect = require("chai").expect;
+
+describe("Sentence Scrambler", function () {
+
+    describe("", function () {
+
+        it("should do x y z", function () {
+
+            expect(true).to.equal(true);
+        });
+
+    });
+});
